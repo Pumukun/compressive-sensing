da3ecc40d7590f2961b68dc5bb1d5b173fc130f3
pumukun <demgrisha@gmail.com>
2025-03-19 13:50:14 +0300

    Добавил бд для результатов

---
+++ test/db/__init__.py
@@ -0,0 +1,19 @@
+from .db import (
+    connect_db,
+    create_table,
+    add_result,
+    get_all_results,
+    get_result_by_id,
+    update_result,
+    delete_result
+)
+
+__all__ = [
+    'connect_db',
+    'create_table',
+    'add_result',
+    'get_all_results',
+    'get_result_by_id',
+    'update_result',
+    'delete_result'
+]
